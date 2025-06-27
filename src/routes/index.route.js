ff8522e621db3b32a14830c8f78ba754d869e298
wilder78 <173951485+wilder78@users.noreply.github.com>
2025-06-26 19:14:09 -0500

    Base principal para llamar consultar productos.

--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -1,11 +1,9 @@
-// src/routes/index.route.js
 import { Router } from "express";
-import { ProductsRouter } from "./products.route.js";
+import ProductsRouter from "./products.route.js";
 
 const router = Router();
 
 // Montar rutas hijas
 router.use("/products", ProductsRouter);
 
-// Exportar como default (💡 lo que causaba el error)
 export default router;
--- src/server.js
+++ src/server.js
@@ -1,13 +1,32 @@
 import initApp from "./app/index.app.js";
 import { config } from "./config/ruta.config.js";
 
+// Inicializa la aplicación
 const app = initApp();
-const PORT = config.PORT || 8080;
 
+// Puerto de escucha
+const PORT = config.PORT || 8000;
+
+// Inicia el servidor
 const server = app.listen(PORT, () => {
-  console.info(`🚀 Server running at: http://localhost:${PORT}`);
+  console.info(`🚀 Servidor iniciado en: http://127.0.0.1:${PORT}`);
 });
 
+// Manejo de errores del servidor
 server.on("error", (error) => {
-  console.error("Server error:", error);
+  console.error("❌ Error en el servidor:", error.message || error);
 });
+
+// import initApp from "./app/index.app.js";
+// import { config } from "./config/ruta.config.js";
+
+// const app = initApp();
+// const PORT = config.PORT || 8080;
+
+// const server = app.listen(PORT, () => {
+//   console.info(`🚀 Server running at: http://localhost:${PORT}`);
+// });
+
+// server.on("error", (error) => {
+//   console.error("Server error:", error);
+// });
